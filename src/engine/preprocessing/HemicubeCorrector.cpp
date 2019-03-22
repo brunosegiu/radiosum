92465bac76ee61800542c4f3461ea4403d4dc561
Bruno Sena <brunosena@tryolabs.com>
2019-03-22 18:21:13 -0300

    Fix hemicube factor

--- src/engine/preprocessing/HemicubeCorrector.cpp
+++ src/engine/preprocessing/HemicubeCorrector.cpp
@@ -6,22 +6,24 @@
 
 HemicubeCorrector::HemicubeCorrector(GLuint width) {
 	std::vector<GLfloat> topData(width * width, 0.0f);
+	const double halfWidth = (.5 * GLdouble(width));
+	const double normalizeTerm = 1.0 / GLdouble(width * width * 3);
 	for (GLuint ix = 0; ix < width; ix++) {
 		for (GLuint iy = 0; iy < width; iy++) {
-			GLdouble x = (GLdouble(ix) - (.5 * GLdouble(width))) / (GLdouble(width));
-			GLdouble y = (GLdouble(iy) - (.5 * GLdouble(width))) / (GLdouble(width));
+			GLdouble x = (GLdouble(ix) - halfWidth) / halfWidth;
+			GLdouble y = (GLdouble(iy) - halfWidth) / halfWidth;
 			GLdouble sqrtDiv = (x * x + y * y + 1.0);
-			topData[ix * width + iy] = 1 / (M_PI*(sqrtDiv * sqrtDiv));
+			topData[ix * width + iy] = normalizeTerm / (M_PI*(sqrtDiv * sqrtDiv));
 		}
 	}
 
 	std::vector<GLfloat> sideData(width * (width / 2), 0.0f);
 	for (GLuint iy = 0; iy < width; iy++) {
 		for (GLuint iz = 0; iz < width / 2; iz++) {
-			GLdouble y = GLdouble(iy) - (.5 * GLdouble(width)) / (GLdouble(width));;
-			GLdouble z = GLdouble(iz) / (.5 * GLdouble(width));;
+			GLdouble y = (GLdouble(iy) - halfWidth) / halfWidth;
+			GLdouble z = GLdouble(iz) / halfWidth;
 			GLdouble sqrtDiv = (y * y + z * z + 1.0);
-			sideData[iy * width / 2 + iz] = z / (M_PI*(sqrtDiv * sqrtDiv));
+			sideData[iy * width / 2 + iz] = normalizeTerm * (z / (M_PI*(sqrtDiv * sqrtDiv)));
 		}
 	}
 
